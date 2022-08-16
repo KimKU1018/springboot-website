b776ea889eb07f87168f153be1f71d2b9f90432e
‘KimKU1018’ <86593582+KimKU1018@users.noreply.github.com>
2022-08-16 22:11:26 +0900

    third commit

---
+++ src/main/java/com/example/springbootwebsite/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.example.springbootwebsite.service.posts;
+
+import com.example.springbootwebsite.domain.posts.PostRepository;
+import com.example.springbootwebsite.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postRepository.save(requestDto.toEntity()).getId();
+    }
+}
